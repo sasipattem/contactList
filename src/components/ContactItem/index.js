5768ed8e004415a02fe426ccb795b3a4ba986faa
sasipattem <nxtwave@ip-10-2-74-63.ap-south-1.compute.internal>
2024-04-11 16:17:58 +0000

    contact detials

---
+++ src/components/ContactItem/index.js
@@ -0,0 +1,34 @@
+import './index.css'
+
+const ContactItem = props => {
+  const {contactDetails, toggleisFavorite} = props
+  const {name, mobileNo, isFavorite, id} = contactDetails
+
+  const starImgUrl = isFavorite
+    ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
+
+  const onClickFavoriteItem = () => {
+    toggleisFavorite(id)
+  }
+  return (
+    <li className="table-row">
+      <div className="table-cell name-column">
+        <p>{name}</p>
+      </div>
+      <hr className="separator" />
+      <div className="table-cell mobile-no-column">
+        <p className="mobile-no-value">{mobileNo}</p>
+        <button
+          type="button"
+          className="favorite-icon-container"
+          onClick={onClickFavoriteItem}
+        >
+          <img src={starImgUrl} className="favorite-icon" alt="star" />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default ContactItem
